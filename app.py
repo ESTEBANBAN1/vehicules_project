df1d208f80e53ec820c22d3274d93a372e6c13e9
EstebanIllan <144294681+ESTEBANBAN1@users.noreply.github.com>
2023-12-31 20:42:13 -0300

    Probando graficos y botonoes nuevos

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+# Encabezado con texto
+st.header('Análisis de Datos de Vehículos en Estados Unidos')
+
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
-# crear una casilla de verificación
-
+    
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
@@ -16,3 +19,28 @@ if hist_button: # al hacer clic en el botón
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+# Gráfico de dispersión
+st.subheader('Gráfico de Dispersión: Precio vs. Año')
+fig_scatter = px.scatter(car_data, x='year', y='price')
+st.plotly_chart(fig_scatter, use_container_width=True)
+
+# Casilla de verificación
+checkbox = st.checkbox('Mostrar Detalles Adicionales')
+if checkbox:
+    # Contenido adicional cuando la casilla está marcada
+    st.write('Detalles adicionales...')
+
+# Otro botón (puedes cambiar a casilla de verificación según tus necesidades)
+button_2 = st.button('Otro Botón')
+if button_2:
+    st.write('Haz clic en el segundo botón')
+
+# Puedes agregar más visualizaciones, botones, casillas de verificación, etc.
+
+# Asegúrate de que haya contenido en general
+if not (hist_button or checkbox or button_2):
+    st.warning('No se ha seleccionado ninguna opción. Seleccione al menos una para visualizar.')
+
+# Asegúrate de cerrar la aplicación de Streamlit
+st.balloons()
