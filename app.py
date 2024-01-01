853c347f219f8729a80cd965bb7626edc2b903a2
EstebanIllan <144294681+ESTEBANBAN1@users.noreply.github.com>
2024-01-01 13:51:01 -0300

    volviendo a lo simple

--- app.py
+++ app.py
@@ -24,20 +24,4 @@ if hist_button: # al hacer clic en el botón
 st.subheader('Gráfico de Dispersión: Precio vs. Año')
 fig_scatter = px.scatter(car_data, x='model_year', y='price')
 st.plotly_chart(fig_scatter, use_container_width=True)
-
-# Encabezado con texto
-st.header('Gráfico de Dispersión Interactivo')
-
-# Botón para cambiar el estilo del gráfico
-button_change_style = st.button('Cambiar Estilo del Gráfico')
-
-# Gráfico de dispersión inicial
-fig_scatter = px.scatter(car_data, x='model_year', y='price', title='Gráfico de Dispersión')
-st.plotly_chart(fig_scatter, use_container_width=True)
-
-# Sección condicional basada en el botón
-if button_change_style:
-    # Cambiar el estilo del gráfico a línea si el botón se presiona
-    fig_line = px.line(car_data, x='model_year', y='price', title='Gráfico de Línea')
-    st.plotly_chart(fig_line, use_container_width=True)
     
